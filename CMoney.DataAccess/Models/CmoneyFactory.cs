1bb48e618c143b10a13839203a50298c0c61c48a
MickeyShiue <e031304@gmail.com>
2020-12-26 09:54:13 +0800

    EF Core Code First
    使用工具產生 POCO Entity
    設定EF Core Factory

---
+++ CMoney.DataAccess/Models/CmoneyFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CMoney.DataAccess.Lib.Models
+{
+    public class CmoneyFactory : IDesignTimeDbContextFactory<CmoneyContext>
+    {
+        public CmoneyContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CmoneyContext>();
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=CMoney;Integrated Security=True");
+
+            return new CmoneyContext(optionsBuilder.Options);
+        }
+    }
+}
