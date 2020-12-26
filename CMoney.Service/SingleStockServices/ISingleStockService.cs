a7b6f3cb45d226cb5066f26ec1a016347a19976d
MickeyShiue <e031304@gmail.com>
2020-12-26 10:18:32 +0800

    替 Service 建立 ISingleStockService 並實作
    注入 dbContext 與 Repository

---
+++ CMoney.Service/SingleStockServices/ISingleStockService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using CMoney.DataAccess.Lib.Models;
+
+namespace CMoney.Service.Lib.SingleStockServices
+{
+    public interface ISingleStockService
+    {
+        IEnumerable<SingleStock> GetAll(Expression<Func<SingleStock, bool>> filter = null,
+            Func<IQueryable<SingleStock>, IOrderedQueryable<SingleStock>> orderBy = null);
+
+        void Create(IEnumerable<SingleStock> singleStocks);
+    }
+}
---
+++ CMoney.Service/SingleStockServices/SingleStockService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using CMoney.DataAccess.Lib.Interface;
+using CMoney.DataAccess.Lib.Models;
+
+namespace CMoney.Service.Lib.SingleStockServices
+{
+    public class SingleStockService : ISingleStockService
+    {
+        private readonly IRepository<SingleStock> _singleStockRepository;
+        private readonly CmoneyContext _context;
+
+        public SingleStockService(CmoneyContext context, IRepository<SingleStock> singleStockRepository)
+        {
+            this._context = context;
+            this._singleStockRepository = singleStockRepository;
+        }
+
+        public IEnumerable<SingleStock> GetAll(Expression<Func<SingleStock, bool>> filter = null,
+            Func<IQueryable<SingleStock>, IOrderedQueryable<SingleStock>> orderBy = null)
+        {
+            return this._singleStockRepository.Find(filter, orderBy);
+        }
+
+        public void Create(IEnumerable<SingleStock> singleStocks)
+        {
+            this._singleStockRepository.CreateRange(singleStocks);
+        }
+    }
+}
